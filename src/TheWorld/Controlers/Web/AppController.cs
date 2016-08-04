8100cf0c517f1e7a342a46179e100b8b0cfcfaa4
Chris Simeone <chrissimeone2@gmail.com>
2016-08-04 11:28:38 -0400

    Add project files.

---
+++ src/TheWorld/Controlers/Web/AppController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorld.Models;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IWorldRepository _repository;
+        private IConfigurationRoot _config;
+        private ILogger<AppController> _logger;
+
+        public AppController(IMailService service, IConfigurationRoot config, IWorldRepository repository,ILogger<AppController> logger)
+        {
+            _mailService = service;
+            _repository = repository;
+            _config = config;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var trips = _repository.GetAllTrips();
+                return View(trips);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to get trips in Index page {ex.Message}");
+                return Redirect("/error");
+            }        
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = _config["MailSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem.");
+                }
+
+                if (_mailService.SendMail(email,email,
+                    $"Contact Page from {model.Name} ({model.Email})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent.  Thanks!";
+                }
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+/* site.js */
+
+/* self executing anonymous function */
+(function () {
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left")
+            $icon.addClass("fa-angle-right")
+        } else {
+            $icon.removeClass("fa-angle-right")
+            $icon.addClass("fa-angle-left")
+        }
+    });
+    
+    //var ele = $("#username");
+    //ele.text("Shawn Wildermuth");
+
+    //var main = $("#main");
+
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "#eee");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+})();
+
+
